d58fa8513e83df09b386dbfd93c93925bb330bd2
Ignacio Losiggio <iglosiggio@gmail.com>
2016-08-26 11:36:24 -0300

    I wasn't right

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
 var exec = require('child_process').exec;
 
-exports.ppid = function(pid, cb) {
+var ppid = exports.ppid = function(pid, cb) {
+    pid = pid || process.pid;
     exec('ps hoppid ' + pid, cb);
 };
 
-exports.wid = function(pid, cb) {
+var wid = exports.wid = function(pid, cb) {
+    pid = pid || process.pid;
     exec('wmctrl -lp | grep ' + pid + ' | cut -d \' \' -f1', cb);
 };
 
-exports.setwmclass = function(wmclass, crawl, pid) {
+var set_wmclass = exports.set_wmclass = function(wmclass, crawl, pid) {
     crawl = crawl || false;
-    pid   = proc  || process.pid;
+    pid   = pid  || process.pid;
 
     /* Debería en realidad ir subiendo en el árbol de procesos hasta encontrar una ventana o el init */
     if(crawl) {
-        get_ppid(pid, function(error, ppid, stderr) {
-            get_wid(ppid, function(error, wid, stderr) {
+        ppid(pid, function(error, ppid, stderr) {
+            wid(ppid, function(error, wid, stderr) {
                 exec('xprop -id' + wid + ' -f WM_CLASS 8s -set WM_CLASS ' + wmclass);
             });
         });
     } else {
-        get_wid(pid, function(error, wid, stderr) {
+        wid(pid, function(error, wid, stderr) {
             exec('xprop -id' + wid + ' -f WM_CLASS 8s -set WM_CLASS ' + wmclass);
         });
     }
