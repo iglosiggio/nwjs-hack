ff0833dcdbe2412cba9c096b7a018d969e131a07
Ignacio Losiggio <iglosiggio@gmail.com>
2016-08-26 12:03:06 -0300

    Now crawls!

--- index.js
+++ index.js
@@ -18,13 +18,26 @@ var set_wmclass = exports.set_wmclass = function(wmclass, crawl, pid) {
     crawl = crawl || false;
     pid   = pid  || process.pid;
 
-    /* Debería en realidad ir subiendo en el árbol de procesos hasta encontrar una ventana o el init */
     if(crawl) {
-        ppid(pid, function(error, ppid, stderr) {
-            wid(ppid, function(error, wid, stderr) {
+        /* Intento conseguir y usar alguna ventana con este pid o llamo al error handler */
+        try_pid = function(pid, errorcb) {
+            wid(pid, function(error, wid, stderr) {
+                if(wid === NaN || typeof(wid) !== 'number') {
+                    return errorcb(pid);
+                }
+
                 exec('xprop -id' + wid + ' -f WM_CLASS 8s -set WM_CLASS ' + wmclass);
             });
-        });
+        };
+
+        /* Consigo el padre e intento usarlo, debería parar en el init, pero bue */
+        try_parent = function(pid) {
+            ppid(pid, function(error, ppid, stderr) {
+                try_pid(pid);
+            }
+        };
+
+        try_pid(pid, try_parent);
     } else {
         wid(pid, function(error, wid, stderr) {
             exec('xprop -id' + wid + ' -f WM_CLASS 8s -set WM_CLASS ' + wmclass);
